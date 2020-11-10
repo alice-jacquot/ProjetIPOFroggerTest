37c9b6e01048d03ffd5713a775f4e5ef6480ab1d
alice <jacquot@lri.fr>
2020-11-10 12:22:04 +0100

    Positionnement de la grenouille en 0,0

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -11,12 +11,18 @@ public class Frog implements IFrog {
 	//gérer : ajouter des attributs ?
 	// Oui ! on veut ses coordonnées : gérer par une case (guidé par "getCase")
 	//et une direction (dans quel sens pointe la grenouille)
+	private Case myPosition;
+
+
+	public Frog(Game game){
+		this.game = game;
+		this.myPosition = new Case(0,0); // pas 0, 0 mais milieu, 0
+	}
 
 
 	@Override
 	public Case getPosition() {
-		//TODO
-		return null;
+		return this.myPosition;
 	}
 
 	@Override
